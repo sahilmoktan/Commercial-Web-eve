2efde762099aa52df563c127617da318f795dc91
Sahil <moktanshail9@gmail.com>
2022-10-19 16:36:36 +0545

    miner updates

--- Clint/src/App.js
+++ Clint/src/App.js
@@ -1,17 +1,41 @@
 import { useState } from "react";
 // import "./App.css";
 // import Button from './Button'
-import Products from "./products";
+// import Products from "./products";
 const App = () => {
   const [name, setName] = useState(" ");
-  const [lotteryNo, setlotteryNo] = useState(" ");
+  const [email, setEmail] = useState(" ");
+  const [password, setPassword] = useState(" ");
+
   const checkInDatabase = () => {
     fetch(`https://127.0.0.1:3000/users/${name}`);
+    fetch(`https://127.0.0.1:3000/users/${email}`);
+    fetch(`https://127.0.0.1:3000/users/${password}`);
   };
+
+  const registerUser = async () => {
+    const requestOption = {
+      method: "POST",
+      headers: { "content-type": "application/json" },
+      body: JSON.stringify({ name: name, email: email, password: password }),
+    };
+    fetch("http://localhost:3000/register", requestOption);
+  };
+
+  // const getUsers = async () => {
+  //   const res = await fetch("http://localhost:3000/users");
+  //   const data = await res.json();
+  //   // setusersList(data.users);
+  //   console.log(data.users);
+  // };
+
+  // useEffect(() => {
+  //   getUsers();
+  // }, []);
 
   return (
     <>
-      <Products />
+      {/* <Products /> */}
       {/* <div>
         <input
           id="name"
@@ -31,6 +55,34 @@ const App = () => {
         <br />
         <button onClick={() => checkInDatabase()}>check</button>
       </div> */}
+      <div>
+        <input
+          id="username"
+          placeholder="Name"
+          onKeyUp={(e) => {
+            setName(e.target.value);
+          }}
+        ></input>{" "}
+        <br />
+        <input
+          id="email"
+          placeholder="Your E-mail"
+          onKeyUp={(e) => {
+            setEmail(e.target.value);
+          }}
+        ></input>{" "}
+        <br />
+        <input
+          id="password"
+          placeholder="Your Password"
+          onKeyUp={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>{" "}
+        <br />
+        <button onClick={() => registerUser()}>Sign In</button>
+        <button onClick={() => checkInDatabase()}>Log In</button>
+      </div>
     </>
   );
 };
